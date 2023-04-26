2bb36df5021830f750ce7aab562dfce6f5bf582d
Saniyah-Davis <tup25222@temple.edu>
2023-04-25 21:01:56 -0400

    changes 4/25

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ screen = pygame.display.set_mode((640,480))
 pygame.display.set_caption('Run, Hooter, Run')
 
 def menu():
-    image = pygame.image.load('')
+    image = pygame.image.load('images/Hooter-Lounging.jpg')
     image = pygame.transform.scale(image, (640,480))
     while True:
         screen.blit(image,(0,0))
@@ -15,11 +15,10 @@ def menu():
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.pos[0] in range(300,325) and event.pos[1] in range(200,228):
-                    #
-                    print('play')
+                    game()
 
 def game():
-    image = pygame.image.load('')
+    image = pygame.image.load('background image temple.jpeg')
     image = pygame.transform.scale(image, (640,480))
     while True:
         screen.blit(image,(0,0))
@@ -28,8 +27,7 @@ def game():
             if event.type == pygame.QUIT:
                 pygame.display.quit()
                 exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.KEYDOWN:
                 if event.pos[0] in range(300,325) and event.pos[1] in range(200,228):
-                    #
-                    print('play')
+                    print('space')
 menu()
