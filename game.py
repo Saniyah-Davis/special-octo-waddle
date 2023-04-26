223efd13b1d9a41390557bbe132b1a317eec8d62
Saniyah-Davis <tup25222@temple.edu>
2023-04-25 20:53:46 -0400

    Update game.py

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ screen = pygame.display.set_mode((640,480))
 pygame.display.set_caption('Run, Hooter, Run')
 
 def menu():
-    image = pygame.image.load('final project\Hooter-Lounging.jpg')
+    image = pygame.image.load('')
     image = pygame.transform.scale(image, (640,480))
     while True:
         screen.blit(image,(0,0))
@@ -18,4 +18,18 @@ def menu():
                     #
                     print('play')
 
+def game():
+    image = pygame.image.load('')
+    image = pygame.transform.scale(image, (640,480))
+    while True:
+        screen.blit(image,(0,0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.pos[0] in range(300,325) and event.pos[1] in range(200,228):
+                    #
+                    print('play')
 menu()
