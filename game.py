e84091c849627814fd2cd88d472ffc887c69b1b2
Saniyah-Davis <tup25222@temple.edu>
2023-04-30 18:41:48 -0400

    4/30 changes

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+from playsound import playsound
+playsound('t-for-temple.mp3')
 
 screen = pygame.display.set_mode((831,519))
 pygame.display.set_caption('Run, Hooter, Run')
@@ -18,7 +20,7 @@ def menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.pos[0] in range(300,400) and event.pos[1] in range(100,200):
                     #play button is 70pi by 70pi
-                    game()
+                    intro()
 
 def game():
     image = pygame.image.load('images/background image temple.jpeg')
@@ -32,11 +34,19 @@ def game():
     #jumpcount = 0
     jump = False
 
-    cherry = pygame.image.load(random.choice(obstacles))
-    cherry = pygame.transform.rotozoom(cherry, 0,0.8)
-    cherry_x = 900
-    cherry_y = 400
-    cherry_speed = .5
+#obstacle 1
+    obs = pygame.image.load(random.choice(obstacles))
+    obs = pygame.transform.rotozoom(obs, 0,0.8)
+    obs_x = 900
+    obs_y = 400
+    obs_speed = random.uniform(.5,1)
+
+#obstacle 2
+    obs2 = pygame.image.load(random.choice(obstacles))
+    obs2 = pygame.transform.rotozoom(obs2, 0,0.8)
+    obs2_x = 1400
+    obs2_y = 400
+    obs2_speed = random.uniform(.5,1)
 
     while True:
         screen.blit(image,(bgx-831,0))
@@ -54,16 +64,34 @@ def game():
             player_y = player_y - 2
             if player_y < 0:
                 player_y = 0
-                    
-        c_rect = screen.blit(cherry,(cherry_x,cherry_y))
-        cherry_x -= cherry_speed
-        if cherry_x < -100:
-            cherry = pygame.image.load(random.choice(obstacles))
-            cherry_x = 900
-            cherry_y = player_y
-            cherry_speed = random.uniform(.5,1)
+    
+        o_rect = screen.blit(obs,(obs_x,obs_y))
+        obs_x -= obs_speed
 
-        if p_rect.colliderect(c_rect):
+        o2_rect = screen.blit(obs2,(obs2_x,obs2_y))
+        obs2_x -= obs2_speed
+
+        """if obs_x < 400:
+            obs2 = pygame.image.load(random.choice(obstacles))
+            obs2_x = 1000
+            obs2_y = random.uniform(0,519)
+            obs2_speed = random.uniform(.5,1)"""
+
+        if obs_x < -100:
+            obs = pygame.image.load(random.choice(obstacles))
+            obs = pygame.transform.rotozoom(obs, 0,0.8)
+            obs_x = 900
+            obs_y = random.uniform(0,400)
+            obs_speed = random.uniform(.5,1)
+
+        if obs2_x < -100:
+            obs2 = pygame.image.load(random.choice(obstacles))
+            obs2 = pygame.transform.rotozoom(obs2, 0,0.8)
+            obs2_x = 1000
+            obs2_y = random.uniform(0,400)
+            obs2_speed = random.uniform(.5,1)
+              
+        if p_rect.colliderect(o_rect) or p_rect.colliderect(o2_rect):
             return
 
         pygame.display.update()
@@ -75,7 +103,20 @@ def game():
                 jump = True
             if event.type == pygame.KEYUP:
                 jump = False
-menu()
 
-def instructions():
-    
+def intro():
+    image = pygame.image.load('images/intro.jpg')
+    image = pygame.transform.scale(image, (831,519))
+    while True:
+        screen.blit(image,(0,0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.pos[0] in range(600,800) and event.pos[1] in range(350,500):
+                    #play button is 70pi by 70pi
+                    game()
+
+menu()
